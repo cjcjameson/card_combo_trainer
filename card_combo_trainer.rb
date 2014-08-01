bd604ebe696cd1c3779d20453748b3d8590037f1
C.J. Jameson <cjcjameson@gmail.com>
2014-07-31 22:23:12 -0700

    Basic user input and output for North and South hands
    North and South are still hard coded
    The loop never ends
    I'm struggling to test the code so far with RSpec
    But there's something going on here!
    Just use control-D to get out.

--- card_combo_trainer.rb
+++ card_combo_trainer.rb
@@ -1,12 +1,30 @@
+PLAYERS = [:north,:east,:south,:west]
+DECLARER = :south
+DUMMY = :north
+
 class CardComboTrainer
 	attr_reader :welcomed
 	def initialize(argv = [])
 		@welcomed = false
 		if argv.include?("--help") || argv.include?("-h")
-			View.help;	return
+			View.help; return
 		end
 		@welcomed = View.welcome
-	end	
+		loop do
+			hand = Hand.new
+			View.display_card_combo(hand)
+		end
+	end
+end
+
+class Hand #Model of a hand
+	attr_reader :north, :south
+	def initialize
+		puts "please enter the cards in north's hand"
+		@north = gets.chomp
+		puts "please enter the cards in south's hand"
+		@south = gets.chomp
+	end
 end
 
 class View
@@ -14,6 +32,8 @@ class View
 		puts "Welcome to the Card Combination Trainer!"
 		puts "Practice your bridge declarer skills by focusing on"
 		puts "the critical card combinations you want to practice"
+		puts ""
+		puts "Start by entering the cards that are in the North and South hands"
 		true
 	end
 
@@ -23,6 +43,12 @@ class View
 		puts ""
 		puts "Available command-line arguments:"
 		puts "--help, -h  display this help file"
+	end
+
+	def self.display_card_combo(hand)
+		puts "North holds " + hand.north if hand.north
+		puts ""
+		puts "South holds " + hand.south if hand.south
 	end
 end
 
